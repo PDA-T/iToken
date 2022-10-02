f2ecf1c78859af4be82e21a4720d895dc9bfcaad
DESKTOP-PCMVKTV\admin <1581109028@qq.com>
2022-10-03 01:16:40 +0800

    service-admin mapper

---
+++ iToken-service-admin/src/main/java/com/pda/itoken/service/admin/mapper/TbSysUserMapper.java
@@ -0,0 +1,7 @@
+package com.pda.itoken.service.admin.mapper;
+
+import com.pda.itoken.service.admin.domain.TbSysUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbSysUserMapper extends MyMapper<TbSysUser> {
+}
