92f512844b3d92c4ad450eec9cdaca557cb167bc
PDA-T <1581109028@qq.com>
2022-10-10 21:04:37 +0800

    设置Bean优先级

--- iToken-common-service/src/main/java/com/pda/itoken/common/mapper/TbPostsPostMapper.java
+++ iToken-common-service/src/main/java/com/pda/itoken/common/mapper/TbPostsPostMapper.java
@@ -3,8 +3,10 @@ package com.pda.itoken.common.mapper;
 import com.pda.itoken.common.domain.TbPostsPost;
 import com.pda.itoken.common.utils.RedisCache;
 import org.apache.ibatis.annotations.CacheNamespace;
+import org.springframework.context.annotation.Primary;
 import tk.mybatis.mapper.MyMapper;
 
+@Primary
 @CacheNamespace(implementation = RedisCache.class)// 二级缓存
 public interface TbPostsPostMapper extends MyMapper<TbPostsPost> {
 }
